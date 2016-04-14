80a8d322379bb9a47932b2ce63ba16f42db61277
Alexander Polyakov <apolyakov@beget.ru>
2016-04-15 00:30:49 +0300

    Split netmap things into submodule

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
-extern crate netmap_sys;
 extern crate libc;
 extern crate pnet;
 
 use std::env;
-use std::mem;
-use std::ptr;
 use std::process;
-use std::slice;
 use std::thread;
-use std::ffi::CString;
-use netmap_sys::netmap;
-use netmap_sys::netmap_user;
 
 use std::net::IpAddr;
 
@@ -18,148 +11,16 @@ use pnet::packet::Packet;
 use pnet::packet::ethernet::{EthernetPacket, EtherTypes};
 use pnet::packet::ip::{IpNextHeaderProtocol, IpNextHeaderProtocols};
 use pnet::packet::ipv4::Ipv4Packet;
-use pnet::packet::ipv6::Ipv6Packet;
-use pnet::packet::udp::UdpPacket;
 use pnet::packet::tcp::TcpPacket;
 
-#[derive(Debug)]
-enum Action {
-    Drop,
-    Forward,
-}
+mod netmap;
+use netmap::{Action,NetmapDescriptor};
 
 // helpers
 fn get_cpu_count() -> usize {
     unsafe { 
         libc::sysconf(libc::_SC_NPROCESSORS_ONLN) as usize
     } 
-}
-
-#[derive(Debug)]
-struct NetmapError {
-    msg: String,
-}
-
-impl NetmapError {
-    fn new(msg: String) -> Self {
-        NetmapError { msg: msg }
-    }
-}
-
-struct NetmapDescriptor {
-    raw: *mut netmap_user::nm_desc
-}
-
-impl NetmapDescriptor {
-    pub fn new(iface: &str) -> Result<Self, NetmapError> {
-        let base_nmd: netmap::nmreq = unsafe { mem::zeroed() };
-        let netmap_iface = CString::new(format!("netmap:{}", iface)).unwrap();
-
-        let netmap_desc = unsafe { netmap_user::nm_open(netmap_iface.as_ptr(), &base_nmd, 0, ptr::null()) };
-        if netmap_desc == ptr::null_mut() {
-            return Err(NetmapError::new(format!("Can't open {:?}", netmap_iface)));
-        }
-        Ok(NetmapDescriptor {
-            raw: netmap_desc
-        })
-    }
-
-    pub fn clone_ring(&self, ring: u16) -> Result<Self,NetmapError> {
-        let mut nm_desc_raw = unsafe { (*self.raw).clone() };
-
-        /* XXX: check that we opened it with ALL_NIC before */
-        nm_desc_raw.req.nr_flags = netmap::NR_REG_ONE_NIC as u32;
-        nm_desc_raw.req.nr_ringid = ring;
-
-        let ifname = unsafe { CString::from_raw((*self.raw).req.nr_name.as_mut_ptr()).into_string().unwrap() };
-        let netmap_ifname = CString::new(format!("netmap:{}", ifname)).unwrap();
-
-        let netmap_desc = unsafe {
-            netmap_user::nm_open(netmap_ifname.as_ptr(),
-                                 ptr::null(),
-                                 netmap_user::NM_OPEN_NO_MMAP as u64 | netmap_user::NM_OPEN_IFNAME as u64,
-                                 self.raw)
-        };
-        if netmap_desc == ptr::null_mut() {
-            return Err(NetmapError::new(format!("Can't open ring {}", ring)));
-        }
-        Ok(NetmapDescriptor {
-            raw: netmap_desc
-        })
-    }
-
-    pub fn get_rx_rings_count(&self) -> u16 {
-        unsafe { (*self.raw).req.nr_rx_rings }
-    }
-
-    pub fn get_tx_rings_count(&self) -> u16 {
-        unsafe { (*self.raw).req.nr_tx_rings }
-    }
-
-    pub fn get_flags(&self) -> u32 {
-        unsafe { (*self.raw).req.nr_flags }
-    }
-
-    /// Returns first and last RX ring
-    pub fn get_rx_rings(&self) -> (u16,u16) {
-        unsafe { ((*self.raw).first_rx_ring, (*self.raw).last_rx_ring) }
-    }
-
-    pub fn get_tx_rings(&self) -> (u16,u16) {
-        unsafe { ((*self.raw).first_tx_ring, (*self.raw).last_tx_ring) }
-    }
-
-    pub fn poll(&mut self, handler: fn(&[u8]) -> Action) {
-        let fd = unsafe { (*self.raw).fd };
-        let mut pollfd: libc::pollfd = unsafe { mem::zeroed() };
-        let mut rx_ring: *mut netmap::netmap_ring = ptr::null_mut();
-        let nifp = unsafe { (*self.raw).nifp };
-
-        pollfd.fd = fd;
-        pollfd.events = libc::POLLIN;
-
-        println!("before poll");
-        let rv = unsafe { libc::poll(&mut pollfd, 1, 1000) };
-        println!("poll: {}", rv);
-
-        let (first, last) = self.get_rx_rings();
-        for ring in first..last+1 {
-            println!("ring {}", ring);
-            rx_ring = unsafe { netmap_user::NETMAP_RXRING(nifp, ring as isize) };
-            if unsafe { netmap::nm_ring_empty(rx_ring) } {
-                continue;
-            }
-            assert!(rx_ring != ptr::null_mut());
-            {
-                let rx_cur = unsafe { (*rx_ring).cur };
-                let slots = unsafe { &(*rx_ring).slot as *const netmap::netmap_slot };
-                let slot = unsafe { slots.offset(rx_cur as isize) };
-                let buf_idx = unsafe { (*slot).buf_idx };
-                let rx_len = unsafe { (*slot).len };
-                let rx_buf = unsafe { netmap_user::NETMAP_BUF(rx_ring, buf_idx as isize) };
-                let rx_slice = unsafe { slice::from_raw_parts::<u8>(rx_buf as *const u8, rx_len as usize) };
-                match handler(rx_slice) {
-                    Action::Drop => {},
-                    Action::Forward => {}, // TODO
-                }
-                unsafe {
-                    (*rx_ring).cur = netmap_user::nm_ring_next(rx_ring, rx_cur);
-                    (*rx_ring).head = (*rx_ring).cur;
-                }
-            }
-        }
-    }
-}
-
-fn handle_udp_packet(source: IpAddr, destination: IpAddr, packet: &[u8]) {
-    let udp = UdpPacket::new(packet);
-
-    if let Some(udp) = udp {
-        println!("UDP Packet: {}:{} > {}:{}; length: {}", source,
-                        udp.get_source(), destination, udp.get_destination(), udp.get_length());
-    } else {
-        println!("Malformed UDP Packet");
-    }
 }
 
 fn handle_tcp_packet(source: IpAddr, destination: IpAddr, packet: &[u8]) {
@@ -176,7 +37,6 @@ fn handle_tcp_packet(source: IpAddr, destination: IpAddr, packet: &[u8]) {
 fn handle_transport_protocol(source: IpAddr, destination: IpAddr,
                              protocol: IpNextHeaderProtocol, packet: &[u8]) {
     match protocol {
-        IpNextHeaderProtocols::Udp  => handle_udp_packet(source, destination, packet),
         IpNextHeaderProtocols::Tcp  => handle_tcp_packet(source, destination, packet),
         _ => println!("Unknown {} packet: {} > {}; protocol: {:?} length: {}",
                 match source { IpAddr::V4(..) => "IPv4", _ => "IPv6" },
@@ -200,12 +60,6 @@ fn handle_ipv4_packet(ethernet: &EthernetPacket) {
     }
 }
 
-fn handle_packet(packet_data: &[u8]) -> Action {
-    let eth = EthernetPacket::new(packet_data).unwrap();
-    handle_ether_packet(&eth);
-    Action::Drop
-}
-
 fn handle_ether_packet(ethernet: &EthernetPacket) {
     println!("{:?}", &ethernet);
     match ethernet.get_ethertype() {
@@ -220,11 +74,17 @@ fn handle_ether_packet(ethernet: &EthernetPacket) {
     }
 }
 
+fn handle_packet(packet_data: &[u8]) -> Action {
+    let eth = EthernetPacket::new(packet_data).unwrap();
+    handle_ether_packet(&eth);
+    Action::Forward
+}
+
 fn rx_loop(netmap: &mut NetmapDescriptor) {
         println!("Rx rings: {:?}", netmap.get_rx_rings());
         println!("Tx rings: {:?}", netmap.get_tx_rings());
         thread::sleep_ms(1000);
-        for _ in (0..10) {
+        for _ in 0..100 {
             netmap.poll(handle_packet);
         }
 }
---
+++ src/netmap.rs
@@ -0,0 +1,139 @@
+extern crate netmap_sys;
+
+use ::libc;
+use std::mem;
+use std::ptr;
+use std::slice;
+use std::ffi::CString;
+use self::netmap_sys::netmap;
+use self::netmap_sys::netmap_user;
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum Action {
+    Drop,
+    Forward,
+    Reply
+}
+
+#[derive(Debug)]
+pub struct NetmapError {
+    msg: String,
+}
+
+impl NetmapError {
+    fn new(msg: String) -> Self {
+        NetmapError { msg: msg }
+    }
+}
+
+pub struct NetmapDescriptor {
+    raw: *mut netmap_user::nm_desc
+}
+
+impl NetmapDescriptor {
+    pub fn new(iface: &str) -> Result<Self, NetmapError> {
+        let base_nmd: netmap::nmreq = unsafe { mem::zeroed() };
+        let netmap_iface = CString::new(format!("netmap:{}", iface)).unwrap();
+
+        let netmap_desc = unsafe { netmap_user::nm_open(netmap_iface.as_ptr(), &base_nmd, 0, ptr::null()) };
+        if netmap_desc == ptr::null_mut() {
+            return Err(NetmapError::new(format!("Can't open {:?}", netmap_iface)));
+        }
+        Ok(NetmapDescriptor {
+            raw: netmap_desc
+        })
+    }
+
+    pub fn clone_ring(&self, ring: u16) -> Result<Self,NetmapError> {
+        let mut nm_desc_raw = unsafe { (*self.raw).clone() };
+
+        /* XXX: check that we opened it with ALL_NIC before */
+        nm_desc_raw.req.nr_flags = netmap::NR_REG_ONE_NIC as u32;
+        nm_desc_raw.req.nr_ringid = ring;
+
+        let ifname = unsafe { CString::from_raw((*self.raw).req.nr_name.as_mut_ptr()).into_string().unwrap() };
+        let netmap_ifname = CString::new(format!("netmap:{}", ifname)).unwrap();
+
+        let netmap_desc = unsafe {
+            netmap_user::nm_open(netmap_ifname.as_ptr(),
+                                 ptr::null(),
+                                 netmap_user::NM_OPEN_NO_MMAP as u64 | netmap_user::NM_OPEN_IFNAME as u64,
+                                 self.raw)
+        };
+        if netmap_desc == ptr::null_mut() {
+            return Err(NetmapError::new(format!("Can't open ring {}", ring)));
+        }
+        Ok(NetmapDescriptor {
+            raw: netmap_desc
+        })
+    }
+
+    pub fn get_rx_rings_count(&self) -> u16 {
+        unsafe { (*self.raw).req.nr_rx_rings }
+    }
+
+    pub fn get_tx_rings_count(&self) -> u16 {
+        unsafe { (*self.raw).req.nr_tx_rings }
+    }
+
+    pub fn get_flags(&self) -> u32 {
+        unsafe { (*self.raw).req.nr_flags }
+    }
+
+    /// Returns first and last RX ring
+    pub fn get_rx_rings(&self) -> (u16,u16) {
+        unsafe { ((*self.raw).first_rx_ring, (*self.raw).last_rx_ring) }
+    }
+
+    pub fn get_tx_rings(&self) -> (u16,u16) {
+        unsafe { ((*self.raw).first_tx_ring, (*self.raw).last_tx_ring) }
+    }
+
+    pub fn poll(&mut self, on_receive: fn(&[u8]) -> Action) {
+        let fd = unsafe { (*self.raw).fd };
+        let mut pollfd: libc::pollfd = unsafe { mem::zeroed() };
+        let mut rx_ring: *mut netmap::netmap_ring;
+        let nifp = unsafe { (*self.raw).nifp };
+
+        pollfd.fd = fd;
+        pollfd.events = libc::POLLIN;
+
+        println!("before poll");
+        let rv = unsafe { libc::poll(&mut pollfd, 1, 1000) };
+        println!("poll: {}", rv);
+
+        let (first, last) = self.get_rx_rings();
+        for ring in first..last+1 {
+            println!("ring {}", ring);
+            rx_ring = unsafe { netmap_user::NETMAP_RXRING(nifp, ring as isize) };
+            if unsafe { netmap::nm_ring_empty(rx_ring) } {
+                continue;
+            }
+            assert!(rx_ring != ptr::null_mut());
+            {
+                let rx_cur = unsafe { (*rx_ring).cur };
+                let slots = unsafe { &(*rx_ring).slot as *const netmap::netmap_slot };
+                let slot = unsafe { slots.offset(rx_cur as isize) as *mut netmap::netmap_slot };
+                let buf_idx = unsafe { (*slot).buf_idx };
+                let rx_len = unsafe { (*slot).len };
+                let rx_buf = unsafe { netmap_user::NETMAP_BUF(rx_ring, buf_idx as isize) };
+                let rx_slice = unsafe { slice::from_raw_parts::<u8>(rx_buf as *const u8, rx_len as usize) };
+                match on_receive(rx_slice) {
+                    Action::Drop => {},
+                    Action::Forward => unsafe {
+                            (*slot).flags |= netmap::NS_FORWARD as u16;
+                            (*rx_ring).flags |= netmap::NR_FORWARD as u32;
+                    },
+                    Action::Reply => {
+                        // TODO
+                    }
+                }
+                unsafe {
+                    (*rx_ring).cur = netmap_user::nm_ring_next(rx_ring, rx_cur);
+                    (*rx_ring).head = (*rx_ring).cur;
+                }
+            }
+        }
+    }
+}
