f740bb12469a9a9ffdb71f2fd1083b8bbd3e6103
Alexander Polakov <apolyakov@beget.ru>
2016-05-24 18:25:20 +0300

    add cookie check

--- src/cookie.rs
+++ src/cookie.rs
@@ -3,16 +3,18 @@ use std::mem;
 use std::net::Ipv4Addr;
 use std::num::Wrapping;
 
-static MSSTAB: [u16;4] = [ 1460, 1300, 1440, 536 ];
+static MSSTAB: [u16;4] = [ 1460, 1440, 1300, 536 ];
 const COOKIEBITS: u32 = 24;	/* Upper bits store count */
 const COOKIEMASK: u32 = ((1 << COOKIEBITS) - 1);
-
 const SHA_WORKSPACE_WORDS: usize = 16;
+
+const MAX_SYNCOOKIE_AGE: u32 = 2;
+
 
 #[inline]
 fn cookie_hash(source_addr: u32, dest_addr: u32, source_port: u16, dest_port: u16,
                 count: u32, c: u32) -> u32 {
-    let mut tmp: [u32; 16 + 5 + SHA_WORKSPACE_WORDS] = unsafe { mem::zeroed() };
+    let mut tmp: [u32; 16 + 5 + SHA_WORKSPACE_WORDS] = unsafe { mem::uninitialized() };
 
     unsafe {
         ::RoutingTable::with_host_config(Ipv4Addr::from(dest_addr.to_be()) /* to_be? */, |hc| {
@@ -76,20 +78,64 @@ pub fn generate_cookie_init_sequence(source_addr: Ipv4Addr, dest_addr: Ipv4Addr,
     (cookie, mssval)
 }
 
-/*
+#[inline]
+fn check_tcp_syn_cookie(cookie: u32, saddr: u32, daddr: u32,
+                        sport: u16, dport: u16, sseq: u32) -> u32 {
+    let diff: Wrapping<u32>;
+    let mut count: Wrapping<u32> = Wrapping(0);
+    unsafe {
+        ::RoutingTable::with_host_config(Ipv4Addr::from(daddr.to_be()) /* to_be? */, |hc| {
+                count = Wrapping(hc.tcp_cookie_time as u32);
+        });
+    }
+
+    let mut cookie = Wrapping(cookie);
+    /* Strip away the layers from the cookie */
+    cookie -= Wrapping(cookie_hash(saddr, daddr, sport, dport, 0, 0)) + Wrapping(sseq);
+
+    /* Cookie is now reduced to (count * 2^24) ^ (hash % 2^24) */
+    diff = (count - (cookie >> COOKIEBITS as usize)) & Wrapping(0xffffffff >> COOKIEBITS);
+    println!("DIFF = {}", diff);
+    if diff >= Wrapping(MAX_SYNCOOKIE_AGE) {
+        return 0xffffffff;
+    }
+    let Wrapping(cd) = count - diff;
+    let Wrapping(res) = (cookie - Wrapping(cookie_hash(saddr, daddr, sport, dport, cd, 1))) & Wrapping(COOKIEMASK);
+    res
+}
+
+#[inline]
+pub fn cookie_check(source_addr: Ipv4Addr, dest_addr: Ipv4Addr,
+                    source_port: u16, dest_port: u16, seq: u32,
+                    cookie: u32) -> u16 {
+    let seq = seq - 1;
+    let source_octets = source_addr.octets();
+    let dest_octets = dest_addr.octets();
+    let mssind = check_tcp_syn_cookie(cookie, oct_to_u32(source_octets).to_be(),
+                        oct_to_u32(dest_octets).to_be(), source_port.to_be(),
+                        dest_port.to_be(), seq);
+    println!("MSSIND = {}", mssind);
+    if mssind < 4 { MSSTAB[mssind as usize] } else { 0 }
+}
+
 #[test]
 fn test_cookie_init() {
-    ::read_uptime();
-    let tcp_cookie_time = 240470;
+    use ::pnet::util::MacAddr;
+    let tcp_cookie_time = 0;
     let source_addr = Ipv4Addr::new(192, 168, 3, 237);
     let dest_addr = Ipv4Addr::new(192, 168, 111, 51);
     let source_port = 51771;
     let dest_port = 9000;
     let seq: u32 = 1646691064;
     let mss = 1460;
-    println!("COOKIE: {:?}", generate_cookie_init_sequence(source_addr, dest_addr, source_port, dest_port, seq.to_be(), mss, tcp_cookie_time));
+    ::RoutingTable::add_host(Ipv4Addr::new(192, 168, 111, 51), MacAddr::new(0, 0, 0, 0, 0, 0));
+    ::RoutingTable::sync_tables();
+    let (cookie, mss) = generate_cookie_init_sequence(source_addr, dest_addr, source_port, dest_port, seq.to_be(), mss, tcp_cookie_time);
+
+    println!("COOKIE: {:?}", (cookie, mss));
+    let mss = cookie_check(dest_addr, source_addr, dest_port, source_port, seq + 1, cookie);
+    println!("CHECK COOKIE: {:?}", mss);
 }
-*/
 
 #[inline]
 pub fn synproxy_init_timestamp_cookie(wscale: u8, sperm: u8, ecn: u8, tcp_time_stamp: u32) -> u32 {
