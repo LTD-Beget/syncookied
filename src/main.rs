9437e9ee0cb24a0be16c434bc56c8314faf3ea93
Alexander Polakov <plhk@sdf.org>
2016-07-08 12:47:20 +0300

    Bump version

--- src/main.rs
+++ src/main.rs
@@ -594,7 +594,7 @@ fn run(config: PathBuf, rx_iface: &str, tx_iface: &str,
 
 fn main() {
     let matches = App::new("syncookied")
-                              .version("0.2.0")
+                              .version("0.2.1")
                               .author("Alexander Polyakov <apolyakov@beget.ru>")
                               .setting(AppSettings::SubcommandsNegateReqs)
                               .subcommand(
