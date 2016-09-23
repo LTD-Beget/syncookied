f45ae632744c6585f15ed21ebd5dbc423e82530b
Alexander Polyakov <apolyakov@beget.com>
2016-09-23 20:54:55 +0300

    Add support for static libpcap

--- build.rs
+++ build.rs
@@ -1,5 +1,13 @@
 extern crate gcc;
 
+use std::env;
+
 fn main() {
+    let static_libpcap_path = env::var_os("STATIC_LIBPCAP_PATH");
+
     gcc::compile_library("libasm.a", &["src/sha1_ssse3_asm.S", "src/sha1.c", "src/csum-partial_64.c"]);
+    if let Some(path) = static_libpcap_path {
+        println!("cargo:rustc-link-lib=static=pcap");
+        println!("cargo:rustc-link-search=native={}", path.into_string().unwrap());
+    }
 }
